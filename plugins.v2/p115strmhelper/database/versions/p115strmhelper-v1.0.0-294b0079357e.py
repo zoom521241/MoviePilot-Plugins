d8c7d3c87dbc30b278fde2472a2ed4a510462adb
zoom521241 <96380355+zoom521241@users.noreply.github.com>
2026-09-18 15:21:39 +0800

    Publish P115StrmHelper 2.8.74 full plugin with transfer fix

---
+++ plugins.v2/p115strmhelper/database/versions/p115strmhelper-v1.0.0-294b0079357e.py
@@ -0,0 +1,28 @@
+"""1.0.0
+
+Revision ID: 294b007932ef57e
+Revises:
+Create Date: 2025-01-20 23:43:40.741251
+
+"""
+
+# revision identifiers, used by Alembic
+version = "1.0.0"
+revision = "294b0079357e"
+down_revision = None
+branch_labels = ("p115strmhelper",)
+depends_on = None
+
+
+def upgrade() -> None:
+    """
+    初始化
+    """
+    pass
+
+
+def downgrade() -> None:
+    """
+    回滚
+    """
+    pass
