ab48d74e473c912cd8c48db4d0f13b7d60cdf5fa
zoom521241 <96380355+zoom521241@users.noreply.github.com>
2026-09-18 15:22:33 +0800

    Publish P115StrmHelper 2.8.74 full plugin with transfer fix

---
+++ plugins.v2/p115strmhelper/db_manager/oper.py
@@ -0,0 +1,7 @@
+from .file_oper import FileDbHelper
+from .life_event import LifeEventDbHelper
+from .moviepilot_transfer import TransferHBOper
+from .open_file_oper import OpenFileOper
+
+
+__all__ = ["FileDbHelper", "LifeEventDbHelper", "TransferHBOper", "OpenFileOper"]
