5c6a1b6dda392845eb4fdd75389bb59cf403c690
zoom521241 <96380355+zoom521241@users.noreply.github.com>
2026-09-18 15:22:10 +0800

    Publish P115StrmHelper 2.8.74 full plugin with transfer fix

---
+++ plugins.v2/p115strmhelper/db_manager/models/__init__.py
@@ -0,0 +1,8 @@
+from .file import File
+from .folder import Folder
+from .life_event import LifeEvent
+from .open_file import OpenFile
+from .open_folder import OpenFolder
+
+
+__all__ = ["File", "Folder", "LifeEvent", "OpenFile", "OpenFolder"]
